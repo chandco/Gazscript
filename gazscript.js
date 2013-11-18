ec708ee50f4bf7821ee017c0852762d52d3d40c1
Nathan Edwards <nathan.edwards@chandco.net>
2013-11-18 14:39:04 +0000

    Created the plugin

---
+++ gazscript.js
@@ -0,0 +1,2 @@
+// JavaScript Document for Gaz
+
---
+++ gazscript.php
@@ -0,0 +1,59 @@
+<?php
+
+
+
+/**
+
+ * Plugin Name: Gazscript
+ * Plugin URI: http://blog.chandco.net
+ * Description: Let's Gaz & Nathan & Amanda add gravitas to any wordpress site
+ * Version: 1.0
+ * Author: Nathan Edwards
+ * Author URI: mailto:nathan.edwards@chandco.net
+ * License: GPL2
+
+ */
+
+ 
+
+
+
+
+
+
+
+// load our JS
+
+
+
+function load_gazscript()
+
+{
+		wp_enqueue_script('load_gazscript', plugins_url('nathan.js',__FILE__), 'jquery', "0.1", true );
+		wp_enqueue_script('load_gazscript', plugins_url('gazscript.js',__FILE__), 'jquery', "0.1", true );
+		wp_enqueue_style( 'gz_nathan_style', plugins_url('nathan.css',__FILE__) );
+		wp_enqueue_style( 'gz_gaz_style', plugins_url('gaz.css',__FILE__) );
+		wp_enqueue_style( 'gz_amanda_style', plugins_url('amanda.css',__FILE__) );
+		global $post;
+		
+		if ($post->ID == "MY ID")
+		{
+			wp_enqueue_style( 'gz_gaz_style_specific_page', plugins_url('page-id-myid.css',__FILE__) );
+		}
+}
+
+
+
+
+
+
+
+add_action( 'wp_enqueue_scripts', 'load_gazscript' );
+
+
+
+
+
+ 
+
+?>
